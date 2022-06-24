a92bb25850fd7291ff27dd751870d0ca02516336
mkharsun <mikhail.kharsun@dataart.com>
2022-06-24 13:21:48 +0300

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,10 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://mkharsun:wBzSkcIgESFOFSOV@ac-mi8yopc-shard-00-00.xircsoj.mongodb.net:27017,ac-mi8yopc-shard-00-01.xircsoj.mongodb.net:27017,ac-mi8yopc-shard-00-02.xircsoj.mongodb.net:27017/blog-dev?ssl=true&replicaSet=atlas-jbgyej-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
+
+
+//wBzSkcIgESFOFSOV
